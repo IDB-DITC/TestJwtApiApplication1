70133f9ecfe30065006a210e6d401ccd6b96056f
idbditc@gmail.com <idbditc@gmail.com>
2024-03-21 15:57:08 +0600

    Add project files.

---
+++ TestJwtApiApplication1/Data/DemoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJwtApiApplication1.Data
+{
+	public class DemoDbContext : IdentityDbContext
+	{
+		public DemoDbContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
